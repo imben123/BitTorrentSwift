f5b021b00821a39fa78034deed51a7c150cbfb16
Ben Davis <imben123@gmail.com>
2016-01-02 16:33:10 +0000

    Initial Commit

---
+++ BitTorrent/BitTorrent.h
@@ -0,0 +1,19 @@
+//
+//  BitTorrent.h
+//  BitTorrent
+//
+//  Created by Ben Davis on 02/01/2016.
+//  Copyright © 2016 Ben Davis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BitTorrent.
+FOUNDATION_EXPORT double BitTorrentVersionNumber;
+
+//! Project version string for BitTorrent.
+FOUNDATION_EXPORT const unsigned char BitTorrentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BitTorrent/PublicHeader.h>
+
+
