5478025a54f5fdc531f1de043d520c2ab76f4124
Ben Davis <imben123@gmail.com>
2016-03-12 15:56:00 +0000

    commented out old implementation

--- BitTorrent/Bencode/BEncoder+decode.swift
+++ BitTorrent/Bencode/BEncoder+decode.swift
@@ -8,101 +8,106 @@
 
 import Foundation
 
-extension NSData {
-    
-    func rangeAfterByteAtIndex(index: Int) -> NSRange {
-        return NSMakeRange(index+1, self.length-index-1)
-    }
+public extension BEncoder {
+
     
 }
 
-public extension BEncoder {
-    
-    private static let zeroBEncodeString = try! "0:".asciiValue()
-
-    public class func decodeIntegerWithTrailingData(data: NSData) throws -> Int {
-//        let indexOfTerminatingCharacter = self.findIndexOfTerminatingCharacter(data)
-//        if let indexOfTerminatingCharacter = indexOfTerminatingCharacter {
-//            return self.decodeInteger(data[])
-//        } else {
+//extension NSData {
+//    
+//    func rangeAfterByteAtIndex(index: Int) -> NSRange {
+//        return NSMakeRange(index+1, self.length-index-1)
+//    }
+//    
+//}
+//
+//public extension BEncoder {
+//    
+//    private static let zeroBEncodeString = try! "0:".asciiValue()
+//
+//    public class func decodeIntegerWithTrailingData(data: NSData) throws -> Int {
+////        let indexOfTerminatingCharacter = self.findIndexOfTerminatingCharacter(data)
+////        if let indexOfTerminatingCharacter = indexOfTerminatingCharacter {
+////            return self.decodeInteger(data[])
+////        } else {
+////            throw BEncoderException.InvalidBEncode
+////        }
+//        return 0
+//    }
+//    
+//    private class func findIndexOfTerminatingCharacter(data: NSData) -> Int? {
+//        for var i = 0; i < data.length; i++ {
+//            let byte = data[i]
+//            if byte == StructureEndToken[0] {
+//                return i
+//            }
+//        }
+//        return nil
+//    }
+//    
+//    public class func decodeInteger(data: NSData) throws -> Int {
+//        let firstByte = data.subdataWithRange(NSMakeRange(0, 1))
+//        let lastByte  = data.subdataWithRange(NSMakeRange(data.length-1, 1))
+//        let asciiI = try! "i".asciiValue()
+//        let asciiE = try! "e".asciiValue()
+//        if firstByte != asciiI || lastByte != asciiE {
 //            throw BEncoderException.InvalidBEncode
 //        }
-        return 0
-    }
-    
-    private class func findIndexOfTerminatingCharacter(data: NSData) -> Int? {
-        for var i = 0; i < data.length; i++ {
-            let byte = data[i]
-            if byte == StructureEndToken[0] {
-                return i
-            }
-        }
-        return nil
-    }
-    
-    public class func decodeInteger(data: NSData) throws -> Int {
-        let firstByte = data.subdataWithRange(NSMakeRange(0, 1))
-        let lastByte  = data.subdataWithRange(NSMakeRange(data.length-1, 1))
-        let asciiI = try! "i".asciiValue()
-        let asciiE = try! "e".asciiValue()
-        if firstByte != asciiI || lastByte != asciiE {
-            throw BEncoderException.InvalidBEncode
-        }
-        let rangeOfIntegerBytes = NSMakeRange(1, data.length-2)
-        let integerBytes = data.subdataWithRange(rangeOfIntegerBytes)
-        do {
-            return try Int.fromAsciiData(integerBytes)
-        } catch let e as AsciiError where e == AsciiError.Invalid {
-            throw BEncoderException.InvalidBEncode
-        }
-    }
-    
-    public class func decodeByteString(data: NSData) throws -> NSData {
-        let rangeOfByteString = try self.rangeOfByteStringIfValidBEncode(data)
-        return data.subdataWithRange(rangeOfByteString)
-    }
-    
-    private class func rangeOfByteStringIfValidBEncode(data: NSData) throws -> NSRange {
-        
-        guard let colonIndex = self.indexOfDelimiterIfNotZero(data) else {
-            throw BEncoderException.InvalidBEncode
-        }
-        
-        try self.testStringLength(data, colonIndex: colonIndex)
-        
-        return data.rangeAfterByteAtIndex(colonIndex)
-    }
-    
-    private class func indexOfDelimiterIfNotZero(data: NSData) -> Int? {
-        let result = self.indexOfDelimiter(data)
-        if let result = result where result == 0 {
-            return nil
-        }
-        return result
-    }
-    
-    private class func indexOfDelimiter(data: NSData) -> Int? {
-        for i in 0..<data.length {
-            let byte = data.subdataWithRange(NSMakeRange(i, 1))
-            if byte == BEncoder.StringSizeDelimiter {
-                return i
-            }
-        }
-        return nil
-    }
-    
-    private class func testStringLength(data: NSData, colonIndex: Int) throws {
-        let stringLengthData = data.subdataWithRange(NSMakeRange(0, colonIndex))
-        var stringLength = 0
-        do {
-            stringLength = try Int.fromAsciiData(stringLengthData)
-        } catch let e as AsciiError where e == AsciiError.Invalid {
-            throw BEncoderException.InvalidBEncode
-        }
-        
-        if data.length-colonIndex-1 != stringLength {
-            throw BEncoderException.InvalidBEncode
-        }
-    }
-    
-}
+//        let rangeOfIntegerBytes = NSMakeRange(1, data.length-2)
+//        let integerBytes = data.subdataWithRange(rangeOfIntegerBytes)
+//        do {
+//            return try Int.fromAsciiData(integerBytes)
+//        } catch let e as AsciiError where e == AsciiError.Invalid {
+//            throw BEncoderException.InvalidBEncode
+//        }
+//    }
+//    
+//    public class func decodeByteString(data: NSData) throws -> NSData {
+//        let rangeOfByteString = try self.rangeOfByteStringIfValidBEncode(data)
+//        return data.subdataWithRange(rangeOfByteString)
+//    }
+//    
+//    private class func rangeOfByteStringIfValidBEncode(data: NSData) throws -> NSRange {
+//        
+//        guard let colonIndex = self.indexOfDelimiterIfNotZero(data) else {
+//            throw BEncoderException.InvalidBEncode
+//        }
+//        
+//        try self.testStringLength(data, colonIndex: colonIndex)
+//        
+//        return data.rangeAfterByteAtIndex(colonIndex)
+//    }
+//    
+//    private class func indexOfDelimiterIfNotZero(data: NSData) -> Int? {
+//        let result = self.indexOfDelimiter(data)
+//        if let result = result where result == 0 {
+//            return nil
+//        }
+//        return result
+//    }
+//    
+//    private class func indexOfDelimiter(data: NSData) -> Int? {
+//        for i in 0..<data.length {
+//            let byte = data.subdataWithRange(NSMakeRange(i, 1))
+//            if byte == BEncoder.StringSizeDelimiter {
+//                return i
+//            }
+//        }
+//        return nil
+//    }
+//    
+//    private class func testStringLength(data: NSData, colonIndex: Int) throws {
+//        let stringLengthData = data.subdataWithRange(NSMakeRange(0, colonIndex))
+//        var stringLength = 0
+//        do {
+//            stringLength = try Int.fromAsciiData(stringLengthData)
+//        } catch let e as AsciiError where e == AsciiError.Invalid {
+//            throw BEncoderException.InvalidBEncode
+//        }
+//        
+//        if data.length-colonIndex-1 != stringLength {
+//            throw BEncoderException.InvalidBEncode
+//        }
+//    }
+//    
+//}
--- BitTorrentTests/Bencode/BEncoderDecodeTests.swift
+++ BitTorrentTests/Bencode/BEncoderDecodeTests.swift
@@ -10,127 +10,131 @@ import XCTest
 @testable import BitTorrent
 
 class BEncoderDecodeTests: XCTestCase {
-    
-    func testCanDecodeInteger() {
-        encodeIntegerAndTestDecode(0)
-        encodeIntegerAndTestDecode(1)
-        encodeIntegerAndTestDecode(255)
-        encodeIntegerAndTestDecode(99999)
-    }
-    
-    func encodeIntegerAndTestDecode(integer: Int) {
-        let encodedInteger = BEncoder.encodeInteger(integer)
-        decodeIntegerAndCompare(encodedInteger, expectedResult: integer)
-    }
-    
-    func decodeIntegerAndCompare(bEncodedInteger: NSData, expectedResult: Int) {
-        let result = try! BEncoder.decodeInteger(bEncodedInteger)
-        XCTAssertEqual(result, expectedResult)
-    }
-    
-    func testExceptionThrownIfFirstCharacterNoti() {
-        assertExceptionThrown(BEncoderException.InvalidBEncode) {
-            try BEncoder.decodeInteger("x5e".asciiValue())
-        }
-    }
-    
-    func testExceptionThrownIfLastCharacterNote() {
-        assertExceptionThrown(BEncoderException.InvalidBEncode) {
-            try BEncoder.decodeInteger("i5x".asciiValue())
-        }
-    }
-    
-    func testExceptionThrownIfNotValidNumber() {
-        assertExceptionThrown(BEncoderException.InvalidBEncode) {
-            try BEncoder.decodeInteger("ixe".asciiValue())
-            try BEncoder.decodeInteger("i1x1e".asciiValue())
-        }
-    }
-    
-    func testCanDecodeIntegerWithTrailingCharacters() {
-//        let testInteger = 567
-//        let result = try! BEncoder.decodeInteger("i\(testInteger)e some more characters".asciiValue())
-//        XCTAssertEqual(result, testInteger)
-    }
-    
-    // MARK: -
-    
-    func testDecode0ByteString() {
-        let input = try! NSMutableData(data: Character("0").asciiValue())
-            .andData(BEncoder.StringSizeDelimiter)
-        
-        let result = try! BEncoder.decodeByteString(input)
-        
-        XCTAssertEqual(result, NSData())
-    }
-    
-    func testDecode5ByteString() {
-        let byteString = NSData(byteArray: [ 1, 2, 3, 255, 0])
-        let input = try! NSMutableData(data: Character("5").asciiValue())
-            .andData(BEncoder.StringSizeDelimiter)
-            .andData(byteString)
-        
-        let result = try! BEncoder.decodeByteString(input)
-        
-        XCTAssertEqual(result, byteString)
-    }
-    
-    func testDecode10ByteString() {
-        let byteString = NSData(byteArray: [1,2,3,4,5,6,7,8,9,0])
-        let input = try! NSMutableData(data: "10".asciiValue())
-            .andData(BEncoder.StringSizeDelimiter)
-            .andData(byteString)
-        
-        let result = try! BEncoder.decodeByteString(input)
-        
-        XCTAssertEqual(result, byteString)
-    }
-    
-    func testExceptionThrownIfNoDelimiter() {
-        let input = try! NSMutableData(data: Character("1").asciiValue())
-            .andData(NSData(byteArray: [ 5 ]))
-        
-        assertExceptionThrown(BEncoderException.InvalidBEncode) {
-            let _ = try BEncoder.decodeByteString(input)
-        }
-        
-    }
-    
-    func testExceptionThrownIfStringLengthMissing() {
-        let input = NSMutableData(data: BEncoder.StringSizeDelimiter)
-            .andData(NSData(byteArray: [ 5 ]))
-        
-        assertExceptionThrown(BEncoderException.InvalidBEncode) {
-            let _ = try BEncoder.decodeByteString(input)
-        }
-    }
-    
-    func testExceptionThrownIfStringLengthIsNaN() {
-        let input = try! NSMutableData(data: Character("x").asciiValue())
-            .andData(BEncoder.StringSizeDelimiter)
-            .andData(NSData(byteArray: [ 5 ]))
-        
-        assertExceptionThrown(BEncoderException.InvalidBEncode) {
-            let _ = try BEncoder.decodeByteString(input)
-        }
-        
-    }
-    
-    func testExceptionThrownIfStringLengthWrong() {
-        
-        let shortInput = try! NSMutableData(data: Character("5").asciiValue())
-            .andData(BEncoder.StringSizeDelimiter)
-            .andData(NSData(byteArray: [ 1, 2, 3, 255]))
-        
-        let longInput = try! NSMutableData(data: Character("5").asciiValue())
-            .andData(BEncoder.StringSizeDelimiter)
-            .andData(NSData(byteArray: [ 1, 2, 3, 255, 0, 5]))
-        
-        
-        assertExceptionThrown(BEncoderException.InvalidBEncode) {
-            let _ = try BEncoder.decodeByteString(shortInput)
-            let _ = try BEncoder.decodeByteString(longInput)
-        }
-    }
 
 }
+
+//class BEncoderDecodeTests: XCTestCase {
+//    
+//    func testCanDecodeInteger() {
+//        encodeIntegerAndTestDecode(0)
+//        encodeIntegerAndTestDecode(1)
+//        encodeIntegerAndTestDecode(255)
+//        encodeIntegerAndTestDecode(99999)
+//    }
+//    
+//    func encodeIntegerAndTestDecode(integer: Int) {
+//        let encodedInteger = BEncoder.encodeInteger(integer)
+//        decodeIntegerAndCompare(encodedInteger, expectedResult: integer)
+//    }
+//    
+//    func decodeIntegerAndCompare(bEncodedInteger: NSData, expectedResult: Int) {
+//        let result = try! BEncoder.decodeInteger(bEncodedInteger)
+//        XCTAssertEqual(result, expectedResult)
+//    }
+//    
+//    func testExceptionThrownIfFirstCharacterNoti() {
+//        assertExceptionThrown(BEncoderException.InvalidBEncode) {
+//            try BEncoder.decodeInteger("x5e".asciiValue())
+//        }
+//    }
+//    
+//    func testExceptionThrownIfLastCharacterNote() {
+//        assertExceptionThrown(BEncoderException.InvalidBEncode) {
+//            try BEncoder.decodeInteger("i5x".asciiValue())
+//        }
+//    }
+//    
+//    func testExceptionThrownIfNotValidNumber() {
+//        assertExceptionThrown(BEncoderException.InvalidBEncode) {
+//            try BEncoder.decodeInteger("ixe".asciiValue())
+//            try BEncoder.decodeInteger("i1x1e".asciiValue())
+//        }
+//    }
+//    
+//    func testCanDecodeIntegerWithTrailingCharacters() {
+////        let testInteger = 567
+////        let result = try! BEncoder.decodeInteger("i\(testInteger)e some more characters".asciiValue())
+////        XCTAssertEqual(result, testInteger)
+//    }
+//    
+//    // MARK: -
+//    
+//    func testDecode0ByteString() {
+//        let input = try! NSMutableData(data: Character("0").asciiValue())
+//            .andData(BEncoder.StringSizeDelimiter)
+//        
+//        let result = try! BEncoder.decodeByteString(input)
+//        
+//        XCTAssertEqual(result, NSData())
+//    }
+//    
+//    func testDecode5ByteString() {
+//        let byteString = NSData(byteArray: [ 1, 2, 3, 255, 0])
+//        let input = try! NSMutableData(data: Character("5").asciiValue())
+//            .andData(BEncoder.StringSizeDelimiter)
+//            .andData(byteString)
+//        
+//        let result = try! BEncoder.decodeByteString(input)
+//        
+//        XCTAssertEqual(result, byteString)
+//    }
+//    
+//    func testDecode10ByteString() {
+//        let byteString = NSData(byteArray: [1,2,3,4,5,6,7,8,9,0])
+//        let input = try! NSMutableData(data: "10".asciiValue())
+//            .andData(BEncoder.StringSizeDelimiter)
+//            .andData(byteString)
+//        
+//        let result = try! BEncoder.decodeByteString(input)
+//        
+//        XCTAssertEqual(result, byteString)
+//    }
+//    
+//    func testExceptionThrownIfNoDelimiter() {
+//        let input = try! NSMutableData(data: Character("1").asciiValue())
+//            .andData(NSData(byteArray: [ 5 ]))
+//        
+//        assertExceptionThrown(BEncoderException.InvalidBEncode) {
+//            let _ = try BEncoder.decodeByteString(input)
+//        }
+//        
+//    }
+//    
+//    func testExceptionThrownIfStringLengthMissing() {
+//        let input = NSMutableData(data: BEncoder.StringSizeDelimiter)
+//            .andData(NSData(byteArray: [ 5 ]))
+//        
+//        assertExceptionThrown(BEncoderException.InvalidBEncode) {
+//            let _ = try BEncoder.decodeByteString(input)
+//        }
+//    }
+//    
+//    func testExceptionThrownIfStringLengthIsNaN() {
+//        let input = try! NSMutableData(data: Character("x").asciiValue())
+//            .andData(BEncoder.StringSizeDelimiter)
+//            .andData(NSData(byteArray: [ 5 ]))
+//        
+//        assertExceptionThrown(BEncoderException.InvalidBEncode) {
+//            let _ = try BEncoder.decodeByteString(input)
+//        }
+//        
+//    }
+//    
+//    func testExceptionThrownIfStringLengthWrong() {
+//        
+//        let shortInput = try! NSMutableData(data: Character("5").asciiValue())
+//            .andData(BEncoder.StringSizeDelimiter)
+//            .andData(NSData(byteArray: [ 1, 2, 3, 255]))
+//        
+//        let longInput = try! NSMutableData(data: Character("5").asciiValue())
+//            .andData(BEncoder.StringSizeDelimiter)
+//            .andData(NSData(byteArray: [ 1, 2, 3, 255, 0, 5]))
+//        
+//        
+//        assertExceptionThrown(BEncoderException.InvalidBEncode) {
+//            let _ = try BEncoder.decodeByteString(shortInput)
+//            let _ = try BEncoder.decodeByteString(longInput)
+//        }
+//    }
+//
+//}
